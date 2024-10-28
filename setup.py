a647ac282ae72ad77919616cf0b932b6916251d0
Hiren Joshi <hirenj@gmail.com>
2024-10-28 20:03:42 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-mmseqs',
+    version='0.0.1',
+    url='https://github.com/CopenhagenCenterForGlycomics/py-mmseqs.git',
+    author='Hiren Joshi',
+    author_email='hirenj@gmail.com',
+    description='Wrapper around mmseqs2',
+    packages=find_packages(),    
+    install_requires=[],
+)
